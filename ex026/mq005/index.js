6ba271a007d8a89452df4dfcd2fddb937fcbc017
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-04-01 13:22:38 -0300

    ex026 mq005

--- ex026/mq005/index.js
+++ ex026/mq005/index.js
@@ -2,5 +2,17 @@ const menuHamburger = document.getElementById('burger')
 const menu = document.getElementById('menu')
 
 menuHamburger.addEventListener('click', () => {
-    menu.style.display == 'block' ? menu.style.display = 'none' : menu.style.display = 'block'
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
 })
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
